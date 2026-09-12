f6989aa7c606e4a44171e3596840a9a21015dd39
hzxs163 <74693769+hzxs163@users.noreply.github.com>
2026-09-12 14:43:28 +0800

    Update search.js

--- functions/api/search.js
+++ functions/api/search.js
@@ -579,7 +579,7 @@ async function fetchFromCctv10(query, page, sort, waitUntil) {
       console.log(`Cctv10: search2=${search2}`);
 
       // 2) 用当前 search2 搜
-      const searchPath = `/?search2=${search2}&search=${encodeURIComponent(query)}&p=${page}`;
+      const searchPath = `/?search2=7fzxq5y0&search=${encodeURIComponent(query)}&p=${page}`;
       const html = await fetchWithCache(`${domain}${searchPath}`, 3600, waitUntil);
 
       if (!html.includes('torrent-list')) continue;
